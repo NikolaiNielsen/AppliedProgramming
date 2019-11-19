7a649146e9b3c28546d6e34aa48dce576de36968
Nikolai Nielsen <nikolainielsen@outlook.dk>
2019-11-19 17:24:39 +0100

    fix matrix scalar product

--- chapter5/5_6.cpp
+++ chapter5/5_6.cpp
@@ -8,26 +8,32 @@ void Multiply(double *res, double *A, double **B, int ACols, int BRows, int BCol
 
 int main() {
 
-    double res [3] = {};
-    double A [] = {4,7,9};
+    // double res [3] = {};
+    // double A [] = {4,7,9};
+    // double** B = new double*[3];
+    // for (int i = 0; i < 3; i++){
+    //     B[i] = new double[3];
+	// B[i][0] = i * 3 + 1;
+	// B[i][1] = i * 3 + 2;
+	// B[i][2] = i * 3 + 3;
+    // } 
+    // int ACols = 3;
+    // int BCols = 3;
+    // int BRows = 3;
+    // Multiply(res,A,B,ACols,BRows,BCols);
+
+    int rows=3, cols=3;
+    double** res = new double*[3];
     double** B = new double*[3];
     for (int i = 0; i < 3; i++){
+        res[i] = new double[3];
         B[i] = new double[3];
 	B[i][0] = i * 3 + 1;
 	B[i][1] = i * 3 + 2;
 	B[i][2] = i * 3 + 3;
     } 
-    int ACols = 3;
-    int BCols = 3;
-    int BRows = 3;
-    Multiply(res,A,B,ACols,BRows,BCols);
-
-    // int rows=3, cols=3;
-    // double a[rows][cols] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    // double scale = 0.5;
-    // double res[rows][cols] = {{},{},{}};
-    // Multiply(res, a, scale, rows, cols)
-    // std::cout << res[1][1] << "\n";
+    double scale = 0.5;
+    Multiply(res, scale, B, rows, cols);
     return 0;
 };
 
