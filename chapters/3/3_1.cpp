dc96b033049afd71116223c8ba78427516af0857
Nikolai Nielsen <NikolaiNielsen@outlook.dk>
2020-04-20 17:04:32 +0200

    finish 3.1.4

--- chapters/3/3_1.cpp
+++ chapters/3/3_1.cpp
@@ -18,8 +18,41 @@ int main(int argc, char *argv[])
     // figures, the output is in scientific notation, and plus signs are shown
     // for positive numbers.
 
+    // 4:Amend the program so that it does not automatically create a fresh
+    // file x_and_y.dat every time it is run. Have the program first attempt to
+    // open the file x_and_y.dat as an ifstream for reading. If the file can be
+    // successfully opened then, after closing the ifstream, warn the user.
+    // Have the program prompt the user as to whether it should erase the
+    // existing file or append to the existing file.
 
-    std::ofstream write_output("x_and_y.dat");
+    std::ifstream read_file("x_and_y.dat");
+    bool exists = read_file.is_open();
+    read_file.close();
+
+    int ans;
+    if (exists)
+    {
+        std::cout << "file already exists. Erase (1) or append (2)?\n";
+        std::cin >> ans;
+    }
+
+    std::ofstream write_output; // Declare the ofstream, but don't open it yet.
+    if (ans == 1)
+    {
+        std::cout << "Erasing\n";
+        write_output.open("x_and_y.dat"); // open the ofstream in erase mode
+    }
+    else if (ans == 2)
+    {
+        std::cout << "Appending\n";
+        write_output.open("x_and_y.dat", std::ios::app); // append
+    }
+    else
+    {
+        std::cout << "Didn't recognize. Erasing. \n";
+        write_output.open("x_and_y.dat"); // erase
+    }
+
     write_output.setf(std::ios::scientific);    // 3.1.3
     write_output.setf(std::ios::showpos);       // 3.1.3
     write_output.precision(10);                 // 3.1.3
