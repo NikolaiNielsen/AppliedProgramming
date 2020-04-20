7f7be71452a51e7f2515d1dd1f6052a6cc5a4503
Nikolai Nielsen <NikolaiNielsen@outlook.dk>
2020-04-20 17:10:46 +0200

    add and finish 3_2.cpp

---
+++ chapters/3/3_2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char * argv[])
+{
+    std::ifstream read_file("x_and_y.dat");
+    if (!read_file.is_open())
+    {
+        return 1;
+    }
+    int number_of_rows = 0;
+    while(!read_file.eof())
+    {
+        double dummy1, dummy2, dummy3, dummy4;
+        read_file >> dummy1 >> dummy2 >> dummy3 >> dummy4; // read the full line
+
+        // use read_file.fail() to find a reading fail, and only increment
+        // number of rows based on that.
+        if (!read_file.fail())
+        {
+            number_of_rows++;
+        }
+    }
+    std::cout << "Number of rows = " << number_of_rows << "\n";
+    read_file.close();
+    return 0;
+}
