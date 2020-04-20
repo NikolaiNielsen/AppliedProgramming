f084f49c99993bd2606617105dc83f0cacc8c623
Nikolai Nielsen <NikolaiNielsen@outlook.dk>
2020-04-20 19:13:20 +0200

    loooooads of comments for 4_2 and better printing

--- chapters/4/4_2.cpp
+++ chapters/4/4_2.cpp
@@ -2,15 +2,32 @@
 
 int main()
 {
+    // Assign values to two integer variables
     int i=5, j=2;
 
+    // 3 new pointers. p_i and p_j point to i and j, respectively
     int *p_i=&i, *p_j=&j, *p_k;
+
+    // dynamically allocate p_k
     p_k = new int;
-    std::cout << p_i << "\n" << *p_i << "\n" << p_j << "\n" << *p_j << "\n";
+
+    // print out some addresses and references
+    std::cout << "p_i and what it points to (i):\n";
+    std::cout << p_i << ", " << *p_i << "\n";
+    std::cout << "p_j and what it points to (j):\n";
+    std::cout << p_j << ", " << *p_j << "\n";
+
+    // swap the values of i and j, using only pointers!
     *p_k = *p_i;
     *p_i = *p_j;
     *p_j = *p_k;
-    std::cout << p_i << "\n" << *p_i << "\n" << p_j << "\n" << *p_j << "\n";
+
+    // check if it works
+    std::cout << "after swapping values:\n";
+    std::cout << "p_i and what it points to (i):\n";
+    std::cout << p_i << ", " << *p_i << "\n";
+    std::cout << "p_j and what it points to (j):\n";
+    std::cout << p_j << ", " << *p_j << "\n";
 
     return 0;
 }
