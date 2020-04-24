327adf402611d7d735102235743181343e147372
Nikolai Nielsen <NikolaiNielsen@outlook.dk>
2020-04-24 23:18:40 +0200

    finish 6.1.1-6.1.6. Start testing 6.1.7 subfunctions

--- main.cpp
+++ main.cpp
@@ -1,12 +1,64 @@
-#include "assignment2/ComplexNumber.hpp"
 #include <iostream>
+#include "assignment2/ComplexNumber.hpp"
+#include "assignment2/CalculateExponential.hpp"
 
-int main()
+int main(int argc, char *argv[])
 {
-    ComplexNumber z(1, 1), z2(2);
-    ComplexNumber z3(z);
+    // EXERCISE 6.1.1-6.1.6
+    // ComplexNumber z(1, 1), z2(2);
+    // ComplexNumber z3(z);
+    // std::cout << "z2 = " << z2 << ". z3 = " << z3 << "\n";
+    // std::cout << "Real part of z2: " << z2.GetRealPart() 
+    //           << ". Imaginary part: " << z2.GetImaginaryPart() << "\n";
+    // std::cout << "Real part of z2: " << RealPart(z2) << ". Imaginary part: "
+    //           << ImaginaryPart(z2) << "\n";
+    // ComplexNumber z4 = z3.CalculateConjugate();
+    // std::cout << "z4 = " << z4 << "\n";
+    // z4.SetConjugate();
+    // std::cout << "z4 = " << z4 << "\n";
+    // ComplexNumber z5 = z3*z4.CalculateConjugate();
+    // std::cout << "z5 = " << z5 << "\n";
 
-    std::cout << "z2 = " << z2 << " z3 = " << z3 << "\n";
+    // EXERCISE 6.1.7
+    int power = atoi(argv[1]);
+    ComplexNumber **z = new ComplexNumber*[3];
+    for (int i = 0; i < 3; i++)
+    {
+        z[i] = new ComplexNumber[3];
+        z[i][0] = ComplexNumber(i * 3 + 1, i);
+        z[i][1] = ComplexNumber(i * 3 + 2, i);
+        z[i][2] = ComplexNumber(i * 3 + 3, i);
+    }
+
+    ComplexNumber **res = new ComplexNumber *[3];
+    for (int i = 0; i < 3; i++)
+    {
+        res[i] = new ComplexNumber[3];
+    }
+    
+    CalculateExponential(z, power, res);
+
+    // int N = 3;
+    // for (int i = 0; i < N; i++)
+    // {
+    //     for (int j = 0; j < N; j++)
+    //     {
+    //         for (int k = 0; k < N; k++)
+    //         {
+    //             res[i][j] = res[i][j] + z[i][k] * z[k][j];
+    //         }
+    //     }
+    // }
+
+    int size = 3;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            std::cout << res[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
 
     return 0;
 }
