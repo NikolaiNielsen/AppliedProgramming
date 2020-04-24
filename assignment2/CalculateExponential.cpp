4c57c8a86f7ae23806cc18dda71f21c01e130218
Nikolai Nielsen <NikolaiNielsen@outlook.dk>
2020-04-24 22:45:13 +0200

    add first pass at CalculateExponential

---
+++ assignment2/CalculateExponential.cpp
@@ -0,0 +1,81 @@
+#include <cmath>
+#include "ComplexNumber.hpp"
+
+void Multiply(ComplexNumber **res, ComplexNumber **A, ComplexNumber **B)
+{
+    // Matrix-matrix product. (nxm) X (pxq) = (nxq) if m==p
+    int N = 3;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            for (int k = 0; k < N; k++)
+            {
+                res[i][j] = res[i][j] + A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+void Add(ComplexNumber **res, ComplexNumber **A, ComplexNumber **B)
+{
+    int N = 3;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            res[i][j] = A[i][j] * B[i][j];
+        }
+    }
+}
+
+void Divide(ComplexNumber **res, ComplexNumber **A, double B)
+{
+    int N = 3;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            res[i][j] = ComplexNumber(A[i][j].GetRealPart()/B,
+                                      A[i][j].GetImaginaryPart());
+        }
+    }
+}
+
+void ComplexPower(ComplexNumber **res, ComplexNumber **A, int N)
+{
+    res = A;
+    for (int i=1; i<N; i++)
+    {
+        Multiply(res, res, A);
+    }
+}
+
+
+void CalculateExponential(ComplexNumber **A, int nMax, ComplexNumber **res)
+{
+    ComplexNumber **res0 = new ComplexNumber *[3];
+    for (int i = 0; i < 3; i++)
+    {
+        res0[i] = new ComplexNumber[3];
+        res0[i][0] = ComplexNumber();
+        res0[i][1] = ComplexNumber();
+        res0[i][2] = ComplexNumber();
+        res0[i][i] = ComplexNumber(1);
+    }
+    res = res0;
+    for (int i=1; i<nMax; i++)
+    {
+        ComplexNumber **temp = new ComplexNumber *[3];
+        for (int i = 0; i < 3; i++)
+        {
+            temp[i] = new ComplexNumber[3];
+            temp[i][0] = ComplexNumber();
+            temp[i][1] = ComplexNumber();
+            temp[i][2] = ComplexNumber();
+        }
+        ComplexPower(temp, A, i);
+        Divide(temp, temp, tgamma(i+1));
+        Add(res, res, temp);
+    }
+}
---
+++ assignment2/CalculateExponential.hpp
@@ -0,0 +1,8 @@
+#ifndef CALCULATEEXPONENTIAL
+#define CALCULATEEXPONENTIAL
+
+#include "ComplexNumber.hpp"
+
+void CalculateExponential(ComplexNumber **A, int nMax, ComplexNumber **res);
+
+#endif
