65f4698bda63522b1c8c7fe28c79b1ffc72d1852
Nikolai Nielsen <NikolaiNielsen@outlook.dk>
2020-04-26 18:32:30 +0200

    add comments, don't return residual

--- assignment1/3_3.cpp
+++ assignment1/3_3.cpp
@@ -1,61 +1,42 @@
 #include "3_3.h"
-#include <iostream>
 #include <fstream>
 #include <cassert>
 #include <cmath>
 
-// double* linspace(double start, double stop, int N)
-// {
-//     assert(N>1);
-//     double h = (start-stop)/(N-1.);
-//     double x[N];
-//     for (int i=0; i<N; i++)
-//     {
-//         x[i] = start + i*h;
-//     }
-//     return x;
-// }
-
 void implicit_Euler(int N)
 {
+    // We need more than one point to be able to integrate
     assert (N > 1);
 
+    // initialize spacing h
     double h=1/((double)(N)-1);
+
+    // Initialize arrays for x and y, and set their initial values
     double y[N];
     y[0] = 1;
     double x[N];
     x[0] = 0;
-    double res[N];
-    res[0] = exp(x[0]);
 
+    // Optional analytical result.
+    // double res[N];
+    // res[0] = exp(x[0]);
+
+    // Perform simple implicit euler integration
     for (int i = 1; i < N; i++)
     {
         y[i] = y[i-1]/(h+1);
         x[i] = x[i-1] + h;
-        res[i] = exp(-x[i]);
+        // res[i] = exp(-x[i]);
     }
 
-    // write to the file
+    // write to the file - but make sure it is open first.
     std::ofstream write_output("xy.dat");
     assert(write_output.is_open());
     for (int i=0; i<N; i++)
     {
-        write_output << x[i] << "," << y[i] << "," << y[i]-res[i] <<"\n";
+        write_output << x[i] << "," << y[i] << "\n";
+        // write_output << x[i] << "," << y[i] << "," << y[i]-res[i] <<"\n";
     }
     write_output.close();
 
 }
-
-// int main()
-// {
-//     // double start=0;
-//     // double stop=1;
-//     // int N=50;
-//     // double x* = linspace(start, stop, N);
-//     // for (int i=0; i<N; i++)
-//     // {
-//     //     std::cout << x[i];
-//     // }
-//     implicit_Euler(50);
-//     return 0;
-// }
