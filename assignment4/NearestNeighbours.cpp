15c62ee356fed1c899e47d70502fb5a4ceb601f2
Nikolai Nielsen <NikolaiNielsen@outlook.dk>
2020-05-17 15:42:30 +0200

    finish first pass at classify point

--- assignment4/NearestNeighbours.cpp
+++ assignment4/NearestNeighbours.cpp
@@ -2,8 +2,10 @@
 #include <fstream>
 #include <cassert>
 #include "armadillo.hpp"
+#include <cmath>
 
-int classifyPoint(arma::mat data, arma::rowvec point, int k=5);
+int classifyPoint(arma::mat data, arma::rowvec point, arma::vec labels, 
+                  int k=5);
 
 int main()
 {   
@@ -17,23 +19,51 @@ int main()
     xData.load("/home/niko/Dropbox/KU/5 Year/AppliedProgramming/assignment4/dataX.dat", arma::raw_ascii);
     xTest.load("/home/niko/Dropbox/KU/5 Year/AppliedProgramming/assignment4/dataXtest.dat", arma::raw_ascii);
 
-    classifyPoint(xData, xTest.row(0), 5);
+    classifyPoint(xData, xTest.row(0), yData, 5);
     return 0;
 }
 
-int classifyPoint(arma::mat data, arma::rowvec point, int k)
+int classifyPoint(arma::mat data, arma::rowvec point, arma::vec labels, int k)
 {
+    // Subract point from each row
     arma::mat diff(data);
     for (int i=0; i<data.n_rows; i++)
     {
         diff.row(i) -= point;
     }
 
-    for (int i=0; i<34; i++)
+    // square each element
+    arma::mat prod = diff % diff;
+    
+    // sum of each row
+    arma::vec dist = arma::sum(prod, 1);
+
+    // Square root of each element
+    dist.for_each( [](arma::mat::elem_type& val) {sqrt(val);});
+
+    // Allocate matrix of pairs. 
+    arma::vec sorted_labels(labels.n_rows);
+
+    // the first element of indices is the index of the lowest element in
+    // dist. So if indices[0] = 94, then the smallest element in dist is
+    // dist[94].
+    arma::uvec indices = arma::sort_index(dist);
+
+    // To get the label for the point, we sum up the first k labels (sorted
+    // ascending, according to dist), and take the sign of the result as the
+    // label. This can be done since the labels are either 1 or -1.
+    double summer = 0;
+    for (int i=0; i<k; i++)
     {
-        std::cout << data.row(0)[i] << " - " << point[i] << " = "
-                  << diff.row(0)[i]
-                  << "\n";
+        summer += labels[indices[i]];
     }
-    return 0;
+    if (summer > 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return -1;
+    }
+
 }
